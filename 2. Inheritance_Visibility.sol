3cebbfc8476595fe8dd9b70c1594adfd69ef9cff
DanielleRenee <danielle.r.russell@gmail.com>
2017-08-22 14:56:18 -0500

    WIP linking ethereum account to smart contract dApp.

--- 2. Inheritance_Visibility.sol
+++ 2. Inheritance_Visibility.sol
@@ -21,21 +21,31 @@ contract Calculator {
     }
 }
 
+// this is the first option. it points to the first but it is very limited. it is not a copy, just a new pointer.
 
 contract Interface {
     uint public interResult;
 
+
+
     function addNumber(uint num3, uint num4, address calcAddress) returns (uint) {
+
+
         Calculator calc = Calculator(calcAddress);
         return calc.doAddition(num3, num4);
     }
 }
 
+// this is the second option
+creating a new calculator named superCalculator that inherits everything from Calculator. Choosing the IS operator makes this possible here. Other is using the address of the contract and the function structure. IS is an operator, and will cost gas. Look for the list of operators and how muh gas it will cost. 
 
 contract superCalculator is Calculator {
     function getFinalResult() returns (uint) {
         return addResult;
     }
+
+
+// this is calling the super. it is a hybrid model. 
 
     function calcFinalResult(address calcAddress) returns (uint) {
         Calculator calc = Calculator(calcAddress);
@@ -43,3 +53,10 @@ contract superCalculator is Calculator {
     }
 
 }
+
+
+// every user and every contract are single entities. they are the same as individuals. 
+
+// to a certain extent, you can secure your variables
+
+// keep the data and the logic seperate! everytime you create a new contract, you lose all the data. your schema is set in the contract. 
---
+++ notes.sol
@@ -0,0 +1,29 @@
+MetaMask + truffle
+(chrome plugin)
+
+MetaCoin Example Truffle Dapp
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+npm install -g truffle
+npm install -g ethereumjs-testrpc
+
+
+
+install python3
+
+
+
+(0) 553fa178bf945696309b0324a43ba8c244a8e31e2b4593d8c2afd34049c9361f
+(1) 003a29fee6454ed161276df14094b7138927bd7146ad96130964a8a389717fd0
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+npm install -g truffle
+npm install -g ethereumjs-testrpc
+
+
+
+truffle init webpack
+truffle compile
+truffle migrate
